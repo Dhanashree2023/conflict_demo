6161eed50f0c6532f7dcb9354c95cd450be6c244
Dhanashree2023 <Prachi123451@gmail.com>
2023-12-09 08:54:48 +0530

    first

---
+++ src/com/TEST.java
@@ -0,0 +1,14 @@
+package com;
+
+public class TEST {
+
+	public static void main(String[] args) {
+		
+		System.out.println("a");
+		System.out.println("b");
+		System.out.println("c");
+		System.out.println("d");
+		System.out.println("e");
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Amol
+ *
+ */
+module conflict_demo {
+}
